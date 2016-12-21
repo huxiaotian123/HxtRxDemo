f30735ba3a4a722e81d1805ca240d79f22a60b2d
xiaotian <491494883@qq.com>
2016-12-22 00:04:00 +0800

    首页布局

--- app/src/main/java/rx/hxt/rxdemo/MainActivity.java
+++ app/src/main/java/rx/hxt/rxdemo/MainActivity.java
@@ -1,13 +1,28 @@
 package rx.hxt.rxdemo;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(android.R.id.tabs)
+    TabLayout tabLayout;
+    @Bind(R.id.viewPager)
+    ViewPager viewPager;
+    @Bind(R.id.toolBar)
+    Toolbar toolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolBar);
     }
 }
